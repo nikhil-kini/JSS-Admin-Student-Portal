4b08f5fb89c029276e15d7dcd4a469f72bdd67cf
nithya <nithya.728.sri@gmail.com>
2024-10-30 14:51:49 +0530

    new

--- JSS-admin-portal-api/src/main/resources/schema.sql
+++ JSS-admin-portal-api/src/main/resources/schema.sql
@@ -1,6 +1,17 @@
-CREATE TABLE IF NOT EXISTS USERS (
-    id  PRIMARY KEY,                -- Auto-incrementing ID
-    email VARCHAR(100) NOT NULL UNIQUE,   -- User email, must be unique
-    password VARCHAR(100) NOT NULL,       -- Password field
-    role VARCHAR(50) NOT NULL              -- User role (e.g., Admin, User)
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS documents (
+    docId SERIAL PRIMARY KEY,
+    document_type VARCHAR(255),
+    file_name VARCHAR(255),
+    file_type VARCHAR(255),
+    upload_date TIMESTAMP,
+    user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
