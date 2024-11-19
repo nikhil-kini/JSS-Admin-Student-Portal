d25f4e9707466ad6ba63e96803c37c3f583d5383
nithya <nithya.728.sri@gmail.com>
2024-11-19 20:23:02 +0530

    Lesson plan and question bank

--- JSS-admin-portal-api/src/main/java/com/example/adminlogin/repository/RoleRepo.java
+++
@@ -1,7 +0,0 @@
-//package com.example.adminlogin.repository;
-//
-//import com.example.adminlogin.model.Role;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface RoleRepo extends JpaRepository<Role,Long> {
-//}
