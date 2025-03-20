7860f53dbf61e4fd0aa412be07b45a41b895465d
Akshatha <jk@jk.com>
2025-03-20 23:23:12 +0530

    Attendance managemet

--- JSS-admin-portal-ui/src/app/sidemenu/attendance-management/attendance-management.component.ts
+++ JSS-admin-portal-ui/src/app/sidemenu/attendance-management/attendance-management.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+
+export interface Document {
+  docId?: number;
+  documentType: string;
+  fileName: string;
+  fileType: string;
+  uploadDate?: Date;
+  semester: string;
+  documentCategory: string; 
+  documentPath: string;
+}
 
 @Component({
   selector: 'app-attendance-management',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './attendance-management.component.html',
   styleUrl: './attendance-management.component.css'
 })
+
 export class AttendanceManagementComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router,private http: HttpClient) {}
 
   home(){
     this.router.navigate(['/sidemenu/home']);
@@ -40,20 +56,184 @@ export class AttendanceManagementComponent {
     teachingaids(){
       this.router.navigate(['/sidemenu/teaching-aids']);
     }
+    logout() {
+      localStorage.removeItem('isAuthenticated'); 
+      localStorage.removeItem('loginUser');
+      localStorage.removeItem('userId');
+        this.router.navigate(['/auth/login']);
+    }
     personaldocuments(){
+      
       this.router.navigate(['/sidemenu/personal-documents'])
     }
     staffmanagement(){
       this.router.navigate(['/sidemenu/staff-management'])
     }
-    logout() {
-      localStorage.removeItem('isAuthenticated'); 
-    localStorage.removeItem('loginUser');  
-    localStorage.removeItem('userId');
-      this.router.navigate(['/auth/login']);
-    }
-    
+
     subjectmanagement(){
       this.router.navigate(['/sidemenu/subject-management'])
     }
+
+    selectedSemester: string = '';
+    selectedDocumentCategory: string = 'QuestionBank';
+    selectedSemesterInModal: string = '';
+    fileToUpload: File | null = null;
+    showModal: boolean = false;
+    questionBankDocuments: any[] = [];
+    semesters: string[] = ['Sem1', 'Sem2', 'Sem3', 'Sem4', 'Sem5', 'Sem6'];
+    selectedDocumentType = '';
+    uploadDate: Date = new Date();  
+    
+    onFileChange(event: any) {
+      this.fileToUpload = event.target.files[0];
+    }
+  
+    uploadFile() {
+      if (this.fileToUpload && this.selectedSemester && this.selectedDocumentCategory && this.selectedDocumentType) {
+        const formData = new FormData();
+        const fileType = this.fileToUpload.name.split('.').pop()?.toLowerCase(); 
+        formData.append('file', this.fileToUpload);
+        formData.append('fileName', this.fileToUpload.name);
+        formData.append('semester', this.selectedSemester);
+        formData.append('documentCategory', this.selectedDocumentCategory);
+        formData.append('documentType', this.selectedDocumentType);
+        formData.append('fileType',  fileType || 'unknown');
+        formData.append('uploadDate', this.uploadDate.toISOString());
+        formData.append('userEmail', 'admin@example.com'); 
+        this.http.post<any>('http://localhost:8080/api/alldocuments/upload', formData)
+          .subscribe({
+            next: (response) => {
+              alert('File uploaded successfully');
+            },
+            error: (error) => {
+              alert('Error uploading file');
+            }
+          });
+      } else {
+        alert('Please select a file, semester, and document type');
+      }
+    }
+    
+  
+   
+    
+    openFile(doc: any) {
+      const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+      const fileType = doc.fileName.split('.').pop()?.toLowerCase();
+      const viewUrl = `http://localhost:8080/api/alldocuments/viewFile/${doc.semester}/${doc.fileName}`;
+    
+     
+      if (!isAuthenticated) {
+        
+        alert("You are not logged in. Please log in to access your files.");
+        return;
+      }
+    
+     
+      if (fileType === 'txt' || fileType === 'json' || fileType === 'pdf' || 
+          ['jpg', 'jpeg', 'png', 'gif'].includes(fileType)) {
+        window.open(viewUrl, '_blank');
+      } else if (['doc', 'docx'].includes(fileType)) {
+        
+        const officeOnlineUrl = `https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(viewUrl)}`;
+        window.open(officeOnlineUrl, '_blank');
+      } else if (['xls', 'xlsx'].includes(fileType)) {
+        
+        const officeOnlineUrl = `https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(viewUrl)}`;
+        window.open(officeOnlineUrl, '_blank');
+      } else if (['ppt', 'pptx'].includes(fileType)) {
+        
+        const officeOnlineUrl = `https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(viewUrl)}`;
+        window.open(officeOnlineUrl, '_blank');
+      } else {
+       
+        alert("This file type is not supported for viewing.");
+      }
+    }
+    
+
+  
+    
+
+    viewQuestionBank() {
+      this.showModal = true;
+    }
+  
+    closeModal() {
+      this.showModal = false;
+      
+    }
+  
+    getDocumentsBySemester() {
+      if (this.selectedSemesterInModal) {
+        this.http.get<any[]>(`http://localhost:8080/api/alldocuments/category/${this.selectedDocumentCategory}/${this.selectedSemesterInModal}`)
+          .subscribe({
+            next: (documents) => {
+              this.questionBankDocuments = documents;
+            },
+            error: (error) => {
+              alert('Error fetching documents');
+            }
+          });
+      }
+    }
+  
+  
+  
+
+
+  viewTextFile(doc: any) {
+      this.http.get(doc.downloadUrl, { responseType: 'text' })
+          .subscribe(content => {
+              const blob = new Blob([content], { type: 'text/plain' });
+              const url = URL.createObjectURL(blob);
+              window.open(url, '_blank');
+          });
+  }
+  
+ 
+  viewPdf(doc: any) {
+      window.open(doc.downloadUrl, '_blank');
+  }
+  
+  
+  viewImage(doc: any) {
+      window.open(doc.downloadUrl, '_blank');
+  }
+
+
+
+
+downloadFile(filename: string) {
+  const url = `http://localhost:8080/viewFile/sem1/${filename}`;  
+  const fileType = filename.split('.').pop()?.toLowerCase();  // Get file type
+  const headers = { 'Accept': fileType === 'pdf' ? 'application/pdf' : 'application/octet-stream' };
+
+  this.http.get(url, { responseType: 'blob', headers: headers }).subscribe(response => {
+      const blob = new Blob([response], { type: `application/${fileType}` });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = filename;
+      link.click();
+  }, error => {
+      console.error('Download failed', error);
+  });
 }
+
+
+
+  
+    getDownloadLink(semester: string, fileName: string): string {
+      
+      return `http://localhost:8080/api/alldocuments/download/${semester}/${fileName}`;
+      
+    }
+  }
+
+
+
+
+
+
+
+
--- JSS-student-portal-ui/src/app/sidemenu/attendance-management/attendance-management.component.ts
+++ JSS-student-portal-ui/src/app/sidemenu/attendance-management/attendance-management.component.ts
@@ -1,54 +1,89 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-attendance-management',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './attendance-management.component.html',
   styleUrl: './attendance-management.component.css'
 })
 export class AttendanceManagementComponent {
-  constructor(private router: Router) {}
-
-  home(){
-    this.router.navigate(['/sidemenu/home']);
-
-  }
-  timetable(){
-    this.router.navigate(['/sidemenu/time-table']);
-
-  }
-  // studentsmanagement(){
-  //   this.router.navigate(['/sidemenu/students-management']);
-  // }
-  attendancemanagement(){
-    this.router.navigate(['/sidemenu/attendance-management']);
-  }
-  questionbank(){
-    this.router.navigate(['/sidemenu/question-bank']);
-  }
-    iamodule(){
-      this.router.navigate(['/sidemenu/ia-module']);
+  constructor(private router: Router,private http: HttpClient) {}
+  
+    home(){
+      this.router.navigate(['/sidemenu/home']);
+  
     }
-    feedbacksystem(){
-      this.router.navigate(['/sidemenu/feedback-system']);
+    timetable(){
+      this.router.navigate(['/sidemenu/time-table']);
+  
     }
-    lessonplan(){
-      this.router.navigate(['/sidemenu/lesson-plan']);
-    }
-    teachingaids(){
-      this.router.navigate(['/sidemenu/teaching-aids']);
-    }
-    // personaldocuments(){
-    //   this.router.navigate(['/sidemenu/personal-documents'])
+    // studentsmanagement(){
+    //   this.router.navigate(['/sidemenu/students-management']);
     // }
-    logout() {
-      localStorage.removeItem('isAuthenticated');  
-    localStorage.removeItem('loginUser'); 
-    localStorage.removeItem('userId'); 
-    localStorage.removeItem('username');
-    localStorage.removeItem('Semester');
-      this.router.navigate(['/auth/login']);
+    attendancemanagement(){
+      this.router.navigate(['/sidemenu/attendance-management']);
     }
-}
+    questionbank(){
+      this.router.navigate(['/sidemenu/question-bank']);
+    }
+      iamodule(){
+        this.router.navigate(['/sidemenu/ia-module']);
+      }
+      feedbacksystem(){
+        this.router.navigate(['/sidemenu/feedback-system']);
+      }
+      lessonplan(){
+        this.router.navigate(['/sidemenu/lesson-plan']);
+      }
+      teachingaids(){
+        this.router.navigate(['/sidemenu/teaching-aids']);
+      }
+      logout() {
+        localStorage.removeItem('isAuthenticated');  
+      localStorage.removeItem('loginUser'); 
+      localStorage.removeItem('userId'); 
+      localStorage.removeItem('username');
+      localStorage.removeItem('Semester');
+        this.router.navigate(['/auth/login']);
+      }
+
+      lessonplandocuments: any[] = []; 
+      selectedDocumentCategory: string = 'QuestionBank'; 
+      selectedSemester: string | null = null; 
+  
+      ngOnInit(): void {
+        this.selectedSemester = localStorage.getItem('Semester') || null;
+        
+        console.log('Selected Semester from Local Storage:', this.selectedSemester);
+      
+        if (this.selectedSemester) {
+          this.getDocumentsBySemester();
+        } else {
+          alert('Semester not found in local storage!');
+        }
+      }
+    
+      getDocumentsBySemester() {
+        if (this.selectedSemester) {
+          // Construct the API URL using the selected semester and category
+          const url = `http://localhost:8080/api/alldocuments/category/${this.selectedDocumentCategory}/${this.selectedSemester}`;
+          
+          // Make the HTTP GET request to fetch documents
+          this.http.get<any[]>(url)
+            .subscribe({
+              next: (documents) => {
+                console.log('Fetched Documents:', documents);
+                this.lessonplandocuments = documents; // Store the fetched documents in the array
+              },
+              error: (error) => {
+                console.error('Error fetching documents:', error);
+                alert('Error fetching documents'); // Show an alert on error
+              }
+            });
+        }
+      }
+    }
